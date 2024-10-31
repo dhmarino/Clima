4e50fb9d3d0f2395ec0c37ce7d8f3c7e149755c1
dhmarino <diego.marino@valid.com>
2024-10-31 14:41:59 -0300

    Commit Inicial

---
+++ Clima/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Clima
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxClima = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timerActualizar = new System.Timers.Timer();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClima)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerActualizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxClima
+            // 
+            this.pictureBoxClima.Location = new System.Drawing.Point(12, 9);
+            this.pictureBoxClima.Name = "pictureBoxClima";
+            this.pictureBoxClima.Size = new System.Drawing.Size(148, 117);
+            this.pictureBoxClima.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxClima.TabIndex = 0;
+            this.pictureBoxClima.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 117);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(191, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(191, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(191, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // timerActualizar
+            // 
+            this.timerActualizar.Interval = 600000D;
+            this.timerActualizar.SynchronizingObject = this;
+            this.timerActualizar.Elapsed += new System.Timers.ElapsedEventHandler(this.timerActualizar_Elapsed);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(38, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label8.Location = new System.Drawing.Point(323, 313);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label9.Location = new System.Drawing.Point(38, 288);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label10.Location = new System.Drawing.Point(38, 313);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 25);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(191, 219);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(108, 31);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label12.Location = new System.Drawing.Point(323, 288);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 25);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "label12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 352);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxClima);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clima";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClima)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerActualizar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxClima;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Timers.Timer timerActualizar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Clima/Form1.cs
@@ -0,0 +1,290 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string temp;
+        string feels_like;
+        string temp_min;
+        string temp_max;
+        string pressure;
+        string humidity;
+        string sea_level;
+        string grnd_level;
+        string description = "Sin Datos";
+        string icon = "unknown";
+        double sunriseUnix = 1709386096;
+        double sunsetUnix = 1709322241;
+        DateTime sunrise;
+        DateTime sunset;
+        string ubicacion;
+        int wind;
+        int wind_direction;
+        int visibilidad;
+        int actualizacion = 0;
+        int nubosidad;
+        int counter = 0;
+        bool vez = false;
+        private Timer _timer;
+        string latitud = "-34.6166786"; //@-34.6217612,-58.4311703,14z?
+        string longitud = "-58.4274232";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // Carga inicial de los datos
+            LoadData();
+
+            // Crea un temporizador que se ejecutará cada segundo
+            _timer = new Timer();
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Recarga los datos
+            LoadData();
+        }
+        private void LoadData()
+        {
+
+            if (!vez)
+            {
+                ObtenerValores();
+                vez = true;
+            }
+
+            counter++;
+
+            pictureBoxClima.Image = Image.FromFile("C:\\Desarrollo\\02_Proyectos_C_Sharp\\Clima\\iconsApiWeather\\" + icon + ".png");
+            pictureBoxClima.Refresh();
+            label1.Text = temp + " °C";
+            label1.Refresh();
+            label2.Text = description.ToUpper();
+            label2.Refresh();
+            label3.Text = "ST " + feels_like + " °C";
+            label3.Refresh();
+            label4.Text = "Presion " + pressure + " Hpa";
+            label4.Refresh();
+            label5.Text = "Humedad " + humidity + " %";
+            label5.Refresh();
+            sunrise = UnixTimeStampToDateTime(sunriseUnix);
+            label6.Text = "Salida del Sol: " + sunrise.ToShortTimeString();
+            label6.Refresh();
+            sunset = UnixTimeStampToDateTime(sunsetUnix);
+            label7.Text = "Puesta del Sol: " + sunset.ToShortTimeString();
+            label7.Refresh();
+            label8.Text = ubicacion + " - " + actualizacion;
+            label8.Refresh();
+            string vientodel = "ND";
+            if ((wind_direction >= 0 && wind_direction <= 23) || (wind_direction >= 337 && wind_direction <= 360)) vientodel = "Norte";
+            if (wind_direction >= 24 && wind_direction <= 68) vientodel = "Noreste";
+            if (wind_direction >= 69 && wind_direction <= 113) vientodel = "Este";
+            if (wind_direction >= 114 && wind_direction <= 158) vientodel = "Sudeste";
+            if (wind_direction >= 159 && wind_direction <= 203) vientodel = "Sur";
+            if (wind_direction >= 204 && wind_direction <= 248) vientodel = "Sudoeste";
+            if (wind_direction >= 249 && wind_direction <= 293) vientodel = "Oeste";
+            if (wind_direction >= 294 && wind_direction <= 336) vientodel = "Noroeste";
+            label9.Text = "Viento: " + wind.ToString() + " Km/h del sector " + vientodel;
+            label9.Refresh();
+            label10.Text = "Visibilidad: " + visibilidad / 1000 + " Km";
+            label10.Refresh();
+            label11.Text = "Nubosidad " + nubosidad.ToString() + " %";
+            label11.Refresh();
+            label12.Text = counter.ToString();
+            label12.Refresh();
+            timerActualizar.Enabled = true;
+        }
+
+        public async void ObtenerValores()
+        {
+            string location = await GetLocationByIP();
+            
+            //https://openweathermap.org/api
+            //user: diego.marino@valid.com
+            //pwd: Valid123+-+
+            string key = "1051a9359d698003bb4052b4fc9c46c7";
+            //string latitud = "-34.6166786"; //@-34.6217612,-58.4311703,14z?
+            //string longitud = "-58.4274232";
+
+            //Verificamos si hay proxi
+            Uri testUri = new Uri("http://www.google.com");
+            bool proxyRequired = IsProxyRequired(testUri);
+
+            //Armamos el restClient segun si hay o no proxi
+            RestClient client;
+            if (proxyRequired)
+            {
+                var options = new RestClientOptions()
+                {
+                    Proxy = GetWebProxy()
+                };
+                client = new RestClient(options);
+            }
+            else
+            {
+                client = new RestClient();
+            }
+            
+            //Ejecutamos el request
+            var request = new RestRequest("https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + "&appid=" + key + "&units=metric&lang=es", Method.Get);
+
+            var response = client.Execute(request);
+
+            //Deserializamos la respuesta para poder trabajar con los datos obtenidos
+            dynamic jsonData = JsonConvert.DeserializeObject(response.Content.ToString());
+
+            //Accedemos al campo "documents" que tiene el pdf y a el campo shipmentTrackingNumber que tiene el numero de guia
+            dynamic main = jsonData.main;
+            
+            temp = main.temp.ToString("#.#"); 
+            feels_like = main.feels_like.ToString("#.#");
+            temp_min = main.temp_min;
+            temp_max = main.temp_max;
+            pressure = main.pressure;
+            humidity = main.humidity;
+            sea_level = main.sea_level;
+            grnd_level = main.grnd_level;
+            description = jsonData.weather[0].description;
+            icon = jsonData.weather[0].icon;
+            sunriseUnix = Convert.ToDouble(jsonData.sys.sunrise);
+            sunsetUnix = Convert.ToDouble(jsonData.sys.sunset);
+            ubicacion = jsonData.name;
+            wind = jsonData.wind.speed;
+            wind_direction = jsonData.wind.deg;
+            visibilidad = jsonData.visibility;
+            nubosidad = jsonData.clouds.all;
+            actualizacion++;
+        }
+
+        private async Task<string> GetLocation()
+        {
+            string location = await GetLocationByIP();
+            MessageBox.Show(location, "Ubicación Aproximada");
+            return location;
+        }
+        private async Task<string> GetLocationByIP()
+        {
+            try
+            {
+                //Verificamos si hay proxi
+                Uri testUri = new Uri("http://www.google.com");
+                bool proxyRequired = IsProxyRequired(testUri);
+
+                HttpClientHandler httpClientHandler = new HttpClientHandler();
+                if (proxyRequired)
+                {
+                    // Si hay proxy y no es excluido, configuramos el HttpClientHandler con el proxy
+                    httpClientHandler.Proxy = new WebProxy("172.16.1.26:8080")
+                    {
+                        Credentials = CredentialCache.DefaultNetworkCredentials
+                    };
+                    httpClientHandler.UseProxy = true;
+                }
+                else
+                {
+                    // No se usa proxy
+                    httpClientHandler.UseProxy = false;
+                }
+
+                //// Configuración del HttpClientHandler con el proxy
+                //var httpClientHandler = new HttpClientHandler
+                //{
+                //    Proxy = GetWebProxy(),
+                //    UseProxy = true
+                //};
+
+                using (HttpClient client = new HttpClient(httpClientHandler))
+                {
+                    // Hacer la solicitud al servicio de geolocalización
+                    HttpResponseMessage response = await client.GetAsync("https://ipinfo.io/json");
+                    response.EnsureSuccessStatusCode();
+
+                    // Procesar la respuesta JSON
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(json);
+
+                    // Extraer información de ubicación
+                    string ip = data["ip"].ToString();
+                    string city = data["city"].ToString();
+                    string region = data["region"].ToString();
+                    string country = data["country"].ToString();
+                    string loc = data["loc"].ToString();
+                    
+                    string[] coordinates = loc.Split(',');
+                    latitud = coordinates[0];
+                    longitud = coordinates[1];
+                    
+                    return $"IP: {ip}\nCiudad: {city}\nRegión: {region}\nPaís: {country}\nCoordenadas: {loc}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error obteniendo la ubicación: {ex.Message}";
+            }
+        }
+
+        private void timerActualizar_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            ObtenerValores();
+        }
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+        private IWebProxy GetWebProxy()
+        {
+            //var proxyUrl = "http://proxy-name.companydomain.com:9090/";
+            // First create a proxy object
+            var builder = new UriBuilder("172.16.1.26:8080");
+            var uri = builder.Uri;
+
+            var proxy = new WebProxy()
+            {
+                //Address = new Uri(proxyUrl),
+                Address = uri,
+                BypassProxyOnLocal = false,
+                //UseDefaultCredentials = true, // This uses: Credentials = CredentialCache.DefaultCredentials
+                //* These creds are given to the proxy server, not the web server *
+                Credentials = CredentialCache.DefaultNetworkCredentials
+                //Credentials = new NetworkCredential("diego.marino", "Noviembre2024+-+")
+            };
+            return proxy;
+        }
+
+        public static bool IsProxyRequired(Uri uri)
+        {
+            IWebProxy proxy = WebRequest.GetSystemWebProxy();
+            Uri proxyUri = proxy.GetProxy(uri);
+
+            // Si la URI del proxy es diferente a la URI de destino, significa que se está usando un proxy
+            return !proxyUri.Equals(uri);
+        }
+    }
+}
